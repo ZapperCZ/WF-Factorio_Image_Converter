6cbc25cb105a535381e0534c38679ddfff56073d
ZapperCZ <janfridl@seznam.cz>
2021-06-08 15:08:30 +0200

    Created GUI for the first form, currently subject to change

--- Factorio_Image_Converter/Factorio_Image_Converter/Form1.Designer.cs
+++ Factorio_Image_Converter/Factorio_Image_Converter/Form1.Designer.cs
@@ -29,13 +29,308 @@ namespace Factorio_Image_Converter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.cb_size = new System.Windows.Forms.ComboBox();
+            this.Group_Tools = new System.Windows.Forms.GroupBox();
+            this.btn_color = new System.Windows.Forms.Button();
+            this.btn_palette = new System.Windows.Forms.Button();
+            this.Group_Resolution = new System.Windows.Forms.GroupBox();
+            this.chk_LockRatio = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Width = new System.Windows.Forms.Label();
+            this.Group_Automation = new System.Windows.Forms.GroupBox();
+            this.cb_Pixel = new System.Windows.Forms.ComboBox();
+            this.btn_AutoResolution = new System.Windows.Forms.Button();
+            this.btn_AutoPalette = new System.Windows.Forms.Button();
+            this.btn_Import = new System.Windows.Forms.Button();
+            this.btn_Export = new System.Windows.Forms.Button();
+            this.btn_About = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Group_Tools.SuspendLayout();
+            this.Group_Resolution.SuspendLayout();
+            this.Group_Automation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 343);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(42, 364);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(91, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Original image";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(217, 366);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Result image";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // cb_size
+            // 
+            this.cb_size.FormattingEnabled = true;
+            this.cb_size.Location = new System.Drawing.Point(14, 17);
+            this.cb_size.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cb_size.Name = "cb_size";
+            this.cb_size.Size = new System.Drawing.Size(85, 21);
+            this.cb_size.TabIndex = 3;
+            this.cb_size.Text = "Brush Size";
+            // 
+            // Group_Tools
+            // 
+            this.Group_Tools.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Group_Tools.Controls.Add(this.btn_color);
+            this.Group_Tools.Controls.Add(this.cb_size);
+            this.Group_Tools.Controls.Add(this.btn_palette);
+            this.Group_Tools.Location = new System.Drawing.Point(391, 40);
+            this.Group_Tools.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Tools.Name = "Group_Tools";
+            this.Group_Tools.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Tools.Size = new System.Drawing.Size(325, 45);
+            this.Group_Tools.TabIndex = 4;
+            this.Group_Tools.TabStop = false;
+            this.Group_Tools.Text = "Tools";
+            // 
+            // btn_color
+            // 
+            this.btn_color.Location = new System.Drawing.Point(225, 16);
+            this.btn_color.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_color.Name = "btn_color";
+            this.btn_color.Size = new System.Drawing.Size(84, 23);
+            this.btn_color.TabIndex = 6;
+            this.btn_color.Text = "Color";
+            this.btn_color.UseVisualStyleBackColor = true;
+            // 
+            // btn_palette
+            // 
+            this.btn_palette.Location = new System.Drawing.Point(120, 16);
+            this.btn_palette.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_palette.Name = "btn_palette";
+            this.btn_palette.Size = new System.Drawing.Size(84, 23);
+            this.btn_palette.TabIndex = 5;
+            this.btn_palette.Text = "Palette";
+            this.btn_palette.UseVisualStyleBackColor = true;
+            // 
+            // Group_Resolution
+            // 
+            this.Group_Resolution.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Group_Resolution.Controls.Add(this.chk_LockRatio);
+            this.Group_Resolution.Controls.Add(this.textBox1);
+            this.Group_Resolution.Controls.Add(this.textBox2);
+            this.Group_Resolution.Controls.Add(this.label1);
+            this.Group_Resolution.Controls.Add(this.lb_Width);
+            this.Group_Resolution.Location = new System.Drawing.Point(391, 89);
+            this.Group_Resolution.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Resolution.Name = "Group_Resolution";
+            this.Group_Resolution.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Resolution.Size = new System.Drawing.Size(325, 45);
+            this.Group_Resolution.TabIndex = 5;
+            this.Group_Resolution.TabStop = false;
+            this.Group_Resolution.Text = "Resolution";
+            // 
+            // chk_LockRatio
+            // 
+            this.chk_LockRatio.AutoSize = true;
+            this.chk_LockRatio.Location = new System.Drawing.Point(219, 24);
+            this.chk_LockRatio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chk_LockRatio.Name = "chk_LockRatio";
+            this.chk_LockRatio.Size = new System.Drawing.Size(78, 17);
+            this.chk_LockRatio.TabIndex = 12;
+            this.chk_LockRatio.Text = "Lock Ratio";
+            this.chk_LockRatio.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 17);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(71, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "H:";
+            // 
+            // lb_Width
+            // 
+            this.lb_Width.AutoSize = true;
+            this.lb_Width.Location = new System.Drawing.Point(11, 19);
+            this.lb_Width.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_Width.Name = "lb_Width";
+            this.lb_Width.Size = new System.Drawing.Size(21, 13);
+            this.lb_Width.TabIndex = 0;
+            this.lb_Width.Text = "W:";
+            // 
+            // Group_Automation
+            // 
+            this.Group_Automation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Group_Automation.Controls.Add(this.cb_Pixel);
+            this.Group_Automation.Controls.Add(this.btn_AutoResolution);
+            this.Group_Automation.Controls.Add(this.btn_AutoPalette);
+            this.Group_Automation.Location = new System.Drawing.Point(391, 138);
+            this.Group_Automation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Automation.Name = "Group_Automation";
+            this.Group_Automation.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Automation.Size = new System.Drawing.Size(325, 45);
+            this.Group_Automation.TabIndex = 6;
+            this.Group_Automation.TabStop = false;
+            this.Group_Automation.Text = "Automation";
+            // 
+            // cb_Pixel
+            // 
+            this.cb_Pixel.FormattingEnabled = true;
+            this.cb_Pixel.Location = new System.Drawing.Point(191, 17);
+            this.cb_Pixel.Name = "cb_Pixel";
+            this.cb_Pixel.Size = new System.Drawing.Size(118, 21);
+            this.cb_Pixel.TabIndex = 9;
+            this.cb_Pixel.Text = "Pixel To Blocks";
+            // 
+            // btn_AutoResolution
+            // 
+            this.btn_AutoResolution.Location = new System.Drawing.Point(14, 16);
+            this.btn_AutoResolution.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_AutoResolution.Name = "btn_AutoResolution";
+            this.btn_AutoResolution.Size = new System.Drawing.Size(84, 23);
+            this.btn_AutoResolution.TabIndex = 8;
+            this.btn_AutoResolution.Text = "Resolution";
+            this.btn_AutoResolution.UseVisualStyleBackColor = true;
+            // 
+            // btn_AutoPalette
+            // 
+            this.btn_AutoPalette.Location = new System.Drawing.Point(102, 16);
+            this.btn_AutoPalette.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_AutoPalette.Name = "btn_AutoPalette";
+            this.btn_AutoPalette.Size = new System.Drawing.Size(84, 23);
+            this.btn_AutoPalette.TabIndex = 7;
+            this.btn_AutoPalette.Text = "Palette";
+            this.btn_AutoPalette.UseVisualStyleBackColor = true;
+            // 
+            // btn_Import
+            // 
+            this.btn_Import.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Import.Location = new System.Drawing.Point(391, 362);
+            this.btn_Import.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Import.Name = "btn_Import";
+            this.btn_Import.Size = new System.Drawing.Size(139, 28);
+            this.btn_Import.TabIndex = 7;
+            this.btn_Import.Text = "Import";
+            this.btn_Import.UseVisualStyleBackColor = true;
+            this.btn_Import.Click += new System.EventHandler(this.btn_Import_Click);
+            // 
+            // btn_Export
+            // 
+            this.btn_Export.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Export.Location = new System.Drawing.Point(577, 362);
+            this.btn_Export.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Export.Name = "btn_Export";
+            this.btn_Export.Size = new System.Drawing.Size(139, 28);
+            this.btn_Export.TabIndex = 8;
+            this.btn_Export.Text = "Export";
+            this.btn_Export.UseVisualStyleBackColor = true;
+            // 
+            // btn_About
+            // 
+            this.btn_About.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_About.Location = new System.Drawing.Point(697, 8);
+            this.btn_About.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_About.Name = "btn_About";
+            this.btn_About.Size = new System.Drawing.Size(19, 17);
+            this.btn_About.TabIndex = 9;
+            this.btn_About.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(730, 401);
+            this.Controls.Add(this.btn_About);
+            this.Controls.Add(this.btn_Export);
+            this.Controls.Add(this.btn_Import);
+            this.Controls.Add(this.Group_Automation);
+            this.Controls.Add(this.Group_Resolution);
+            this.Controls.Add(this.Group_Tools);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(744, 274);
+            this.Name = "Form1";
+            this.Text = "Factorio Image Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Group_Tools.ResumeLayout(false);
+            this.Group_Resolution.ResumeLayout(false);
+            this.Group_Resolution.PerformLayout();
+            this.Group_Automation.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox cb_size;
+        private System.Windows.Forms.GroupBox Group_Tools;
+        private System.Windows.Forms.Button btn_palette;
+        private System.Windows.Forms.Button btn_color;
+        private System.Windows.Forms.GroupBox Group_Resolution;
+        private System.Windows.Forms.GroupBox Group_Automation;
+        private System.Windows.Forms.Button btn_Import;
+        private System.Windows.Forms.Button btn_Export;
+        private System.Windows.Forms.Button btn_About;
+        private System.Windows.Forms.Button btn_AutoPalette;
+        private System.Windows.Forms.Button btn_AutoResolution;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_Width;
+        private System.Windows.Forms.CheckBox chk_LockRatio;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_Pixel;
     }
 }
 
--- Factorio_Image_Converter/Factorio_Image_Converter/Form1.cs
+++ Factorio_Image_Converter/Factorio_Image_Converter/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,42 @@ namespace Factorio_Image_Converter
 {
     public partial class Form1 : Form
     {
+        string ImagePath = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Import_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image files (*.txt;*.png)|*.txt;*.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    ImagePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        ImagePath = reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load file with all usable blocks
+            //TODO: Create this file
         }
     }
 }
