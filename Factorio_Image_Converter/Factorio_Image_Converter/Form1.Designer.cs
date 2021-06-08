c2312282ab4c98d7c5c8351bdfd924967e73d2d3
ZapperCZ <janfridl@seznam.cz>
2021-06-08 23:46:50 +0200

    Added some scuffed image loading, needs a lot of polishing

--- Factorio_Image_Converter/Factorio_Image_Converter/Form1.Designer.cs
+++ Factorio_Image_Converter/Factorio_Image_Converter/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace Factorio_Image_Converter
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pb_Image = new System.Windows.Forms.PictureBox();
+            this.rb_Original = new System.Windows.Forms.RadioButton();
+            this.rb_Result = new System.Windows.Forms.RadioButton();
             this.cb_size = new System.Windows.Forms.ComboBox();
             this.Group_Tools = new System.Windows.Forms.GroupBox();
             this.btn_color = new System.Windows.Forms.Button();
@@ -49,56 +49,59 @@ namespace Factorio_Image_Converter
             this.btn_Import = new System.Windows.Forms.Button();
             this.btn_Export = new System.Windows.Forms.Button();
             this.btn_About = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Image)).BeginInit();
             this.Group_Tools.SuspendLayout();
             this.Group_Resolution.SuspendLayout();
             this.Group_Automation.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pb_Image
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pb_Image.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(369, 343);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pb_Image.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_Image.Location = new System.Drawing.Point(8, 8);
+            this.pb_Image.Margin = new System.Windows.Forms.Padding(2);
+            this.pb_Image.Name = "pb_Image";
+            this.pb_Image.Size = new System.Drawing.Size(369, 343);
+            this.pb_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Image.TabIndex = 0;
+            this.pb_Image.TabStop = false;
             // 
-            // radioButton1
+            // rb_Original
             // 
-            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(42, 364);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(91, 17);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Original image";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rb_Original.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rb_Original.AutoSize = true;
+            this.rb_Original.Checked = true;
+            this.rb_Original.Location = new System.Drawing.Point(42, 364);
+            this.rb_Original.Margin = new System.Windows.Forms.Padding(2);
+            this.rb_Original.Name = "rb_Original";
+            this.rb_Original.Size = new System.Drawing.Size(91, 17);
+            this.rb_Original.TabIndex = 1;
+            this.rb_Original.TabStop = true;
+            this.rb_Original.Text = "Original image";
+            this.rb_Original.UseVisualStyleBackColor = true;
+            this.rb_Original.Click += new System.EventHandler(this.Switch_Image);
             // 
-            // radioButton2
+            // rb_Result
             // 
-            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(217, 366);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(86, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Result image";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rb_Result.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.rb_Result.AutoSize = true;
+            this.rb_Result.Location = new System.Drawing.Point(225, 364);
+            this.rb_Result.Margin = new System.Windows.Forms.Padding(2);
+            this.rb_Result.Name = "rb_Result";
+            this.rb_Result.Size = new System.Drawing.Size(86, 17);
+            this.rb_Result.TabIndex = 2;
+            this.rb_Result.Text = "Result image";
+            this.rb_Result.UseVisualStyleBackColor = true;
+            this.rb_Result.Click += new System.EventHandler(this.Switch_Image);
             // 
             // cb_size
             // 
             this.cb_size.FormattingEnabled = true;
             this.cb_size.Location = new System.Drawing.Point(14, 17);
-            this.cb_size.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cb_size.Margin = new System.Windows.Forms.Padding(2);
             this.cb_size.Name = "cb_size";
             this.cb_size.Size = new System.Drawing.Size(85, 21);
             this.cb_size.TabIndex = 3;
@@ -111,9 +114,9 @@ namespace Factorio_Image_Converter
             this.Group_Tools.Controls.Add(this.cb_size);
             this.Group_Tools.Controls.Add(this.btn_palette);
             this.Group_Tools.Location = new System.Drawing.Point(391, 40);
-            this.Group_Tools.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Tools.Margin = new System.Windows.Forms.Padding(2);
             this.Group_Tools.Name = "Group_Tools";
-            this.Group_Tools.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Tools.Padding = new System.Windows.Forms.Padding(2);
             this.Group_Tools.Size = new System.Drawing.Size(325, 45);
             this.Group_Tools.TabIndex = 4;
             this.Group_Tools.TabStop = false;
@@ -122,7 +125,7 @@ namespace Factorio_Image_Converter
             // btn_color
             // 
             this.btn_color.Location = new System.Drawing.Point(225, 16);
-            this.btn_color.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_color.Margin = new System.Windows.Forms.Padding(2);
             this.btn_color.Name = "btn_color";
             this.btn_color.Size = new System.Drawing.Size(84, 23);
             this.btn_color.TabIndex = 6;
@@ -132,7 +135,7 @@ namespace Factorio_Image_Converter
             // btn_palette
             // 
             this.btn_palette.Location = new System.Drawing.Point(120, 16);
-            this.btn_palette.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_palette.Margin = new System.Windows.Forms.Padding(2);
             this.btn_palette.Name = "btn_palette";
             this.btn_palette.Size = new System.Drawing.Size(84, 23);
             this.btn_palette.TabIndex = 5;
@@ -148,9 +151,9 @@ namespace Factorio_Image_Converter
             this.Group_Resolution.Controls.Add(this.label1);
             this.Group_Resolution.Controls.Add(this.lb_Width);
             this.Group_Resolution.Location = new System.Drawing.Point(391, 89);
-            this.Group_Resolution.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Resolution.Margin = new System.Windows.Forms.Padding(2);
             this.Group_Resolution.Name = "Group_Resolution";
-            this.Group_Resolution.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Resolution.Padding = new System.Windows.Forms.Padding(2);
             this.Group_Resolution.Size = new System.Drawing.Size(325, 45);
             this.Group_Resolution.TabIndex = 5;
             this.Group_Resolution.TabStop = false;
@@ -160,7 +163,7 @@ namespace Factorio_Image_Converter
             // 
             this.chk_LockRatio.AutoSize = true;
             this.chk_LockRatio.Location = new System.Drawing.Point(219, 24);
-            this.chk_LockRatio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chk_LockRatio.Margin = new System.Windows.Forms.Padding(2);
             this.chk_LockRatio.Name = "chk_LockRatio";
             this.chk_LockRatio.Size = new System.Drawing.Size(78, 17);
             this.chk_LockRatio.TabIndex = 12;
@@ -170,7 +173,7 @@ namespace Factorio_Image_Converter
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(29, 17);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(71, 20);
             this.textBox1.TabIndex = 2;
@@ -178,7 +181,7 @@ namespace Factorio_Image_Converter
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(133, 17);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(71, 20);
             this.textBox2.TabIndex = 11;
@@ -210,9 +213,9 @@ namespace Factorio_Image_Converter
             this.Group_Automation.Controls.Add(this.btn_AutoResolution);
             this.Group_Automation.Controls.Add(this.btn_AutoPalette);
             this.Group_Automation.Location = new System.Drawing.Point(391, 138);
-            this.Group_Automation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Automation.Margin = new System.Windows.Forms.Padding(2);
             this.Group_Automation.Name = "Group_Automation";
-            this.Group_Automation.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Group_Automation.Padding = new System.Windows.Forms.Padding(2);
             this.Group_Automation.Size = new System.Drawing.Size(325, 45);
             this.Group_Automation.TabIndex = 6;
             this.Group_Automation.TabStop = false;
@@ -230,7 +233,7 @@ namespace Factorio_Image_Converter
             // btn_AutoResolution
             // 
             this.btn_AutoResolution.Location = new System.Drawing.Point(14, 16);
-            this.btn_AutoResolution.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_AutoResolution.Margin = new System.Windows.Forms.Padding(2);
             this.btn_AutoResolution.Name = "btn_AutoResolution";
             this.btn_AutoResolution.Size = new System.Drawing.Size(84, 23);
             this.btn_AutoResolution.TabIndex = 8;
@@ -240,7 +243,7 @@ namespace Factorio_Image_Converter
             // btn_AutoPalette
             // 
             this.btn_AutoPalette.Location = new System.Drawing.Point(102, 16);
-            this.btn_AutoPalette.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_AutoPalette.Margin = new System.Windows.Forms.Padding(2);
             this.btn_AutoPalette.Name = "btn_AutoPalette";
             this.btn_AutoPalette.Size = new System.Drawing.Size(84, 23);
             this.btn_AutoPalette.TabIndex = 7;
@@ -251,7 +254,7 @@ namespace Factorio_Image_Converter
             // 
             this.btn_Import.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_Import.Location = new System.Drawing.Point(391, 362);
-            this.btn_Import.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Import.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Import.Name = "btn_Import";
             this.btn_Import.Size = new System.Drawing.Size(139, 28);
             this.btn_Import.TabIndex = 7;
@@ -263,7 +266,7 @@ namespace Factorio_Image_Converter
             // 
             this.btn_Export.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_Export.Location = new System.Drawing.Point(577, 362);
-            this.btn_Export.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Export.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Export.Name = "btn_Export";
             this.btn_Export.Size = new System.Drawing.Size(139, 28);
             this.btn_Export.TabIndex = 8;
@@ -274,7 +277,7 @@ namespace Factorio_Image_Converter
             // 
             this.btn_About.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_About.Location = new System.Drawing.Point(697, 8);
-            this.btn_About.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_About.Margin = new System.Windows.Forms.Padding(2);
             this.btn_About.Name = "btn_About";
             this.btn_About.Size = new System.Drawing.Size(19, 17);
             this.btn_About.TabIndex = 9;
@@ -291,15 +294,15 @@ namespace Factorio_Image_Converter
             this.Controls.Add(this.Group_Automation);
             this.Controls.Add(this.Group_Resolution);
             this.Controls.Add(this.Group_Tools);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.rb_Result);
+            this.Controls.Add(this.rb_Original);
+            this.Controls.Add(this.pb_Image);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(744, 274);
             this.Name = "Form1";
             this.Text = "Factorio Image Converter";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Image)).EndInit();
             this.Group_Tools.ResumeLayout(false);
             this.Group_Resolution.ResumeLayout(false);
             this.Group_Resolution.PerformLayout();
@@ -311,9 +314,9 @@ namespace Factorio_Image_Converter
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pb_Image;
+        private System.Windows.Forms.RadioButton rb_Original;
+        private System.Windows.Forms.RadioButton rb_Result;
         private System.Windows.Forms.ComboBox cb_size;
         private System.Windows.Forms.GroupBox Group_Tools;
         private System.Windows.Forms.Button btn_palette;
--- Factorio_Image_Converter/Factorio_Image_Converter/Form1.cs
+++ Factorio_Image_Converter/Factorio_Image_Converter/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -14,6 +15,10 @@ namespace Factorio_Image_Converter
     public partial class Form1 : Form
     {
         string ImagePath = "";
+        int DefaultPBWidth;
+        int DefaultPBHeight;
+        Image OriginalImage;
+        Image ResultImage;
 
         public Form1()
         {
@@ -25,30 +30,54 @@ namespace Factorio_Image_Converter
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "Image files (*.txt;*.png)|*.txt;*.png";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Filter = "Image files (*.jpg;*.png)|*.jpg;*.png";    //Add more image formats
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;     //Opens up where the user chose the last file
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    //Get the path of specified file
                     ImagePath = openFileDialog.FileName;
 
-                    //Read the contents of the file into a stream
-                    var fileStream = openFileDialog.OpenFile();
-
-                    using (StreamReader reader = new StreamReader(fileStream))
-                    {
-                        ImagePath = reader.ReadToEnd();
-                    }
+                    //Save the image
+                    OriginalImage = Image.FromFile(ImagePath);
                 }
             }
+            Debug.WriteLine(ImagePath);
+            Switch_Image(null,null);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //Load file with all usable blocks
             //TODO: Create this file
+            DefaultPBWidth = pb_Image.Width;
+            DefaultPBHeight = pb_Image.Height;
+        }
+        private void Switch_Image(object sender, EventArgs e)
+        {
+            Image ImageToSet;
+            if (rb_Original.Checked)
+            {
+                ImageToSet = OriginalImage;
+            }
+            else
+            {
+                ImageToSet = ResultImage;
+            }
+
+            if (ImageToSet != null)
+            {
+                if (ImageToSet.Width > 800 && ImageToSet.Height > 800)
+                {
+                    //Not like this, add difference between image size and default pb size
+                    this.Size = new Size(ImageToSet.Width / 2, ImageToSet.Height / 2);
+                }
+                else
+                {
+                    
+                }
+                pb_Image.Image = ImageToSet;
+            }
         }
     }
 }
